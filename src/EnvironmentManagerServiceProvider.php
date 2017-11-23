a29af393f5907d48a42c081984fa69592eef82d9
Mark Walet <mark.walet@gmail.com>
2017-11-23 21:19:57 +0100

    Boilerplating

---
+++ src/EnvironmentManagerServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MarkWalet\EnvironmentManager;
+
+use Illuminate\Support\ServiceProvider;
+
+class EnvironmentManagerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
